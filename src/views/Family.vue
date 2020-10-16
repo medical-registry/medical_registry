5865c20845eef7ea0febf995f5348c7c75d7be8b
massimo dell'erba <massimo.dellerba@dynamitick.com>
2020-10-16 15:34:38 +0200

    fix pagine disease e disease_parental

--- src/views/Family.vue
+++ src/views/Family.vue
@@ -5,8 +5,9 @@
       <v-timeline  align-top dense>
         <v-timeline-item v-for="item in items" :key="item.id_care" small>
           <v-card class="elevation-3">
-            <v-card-title class="text-center">{{item.disease.name}}
-              ({{item.chronic}})</v-card-title>
+            <v-card-title class="text-center">{{item.disease.name}}</v-card-title>
+            <v-card-title v-if="item.chronic==true" class="text-center">
+                   (Cronico)</v-card-title>
             <v-card-title v-if="!item.to" class="headline capitalized">{{formatDate(item.from)}}
             </v-card-title>
             <v-card-title v-else class="headline capitalized">Dal {{formatDate(item.from)}}
@@ -17,20 +18,24 @@
                   <template v-slot:default>
                    <thead>
                           <tr>
-                            <th v-if="item.parental_type" class="text-left">Grado di Parentela</th>
-                            <th v-if="item.body_impacted" class="text-left">Parte del corpo</th>
-                            <th v-if="item.body_impacted" class="text-left">Scoperto all'età di</th>
-                            <th v-if="item.note" class="text-left">Note</th>
+                            <th v-if="item.parental_type != null" class="text-left">
+                              Grado di Parentela</th>
+                            <th v-if="item.body_impacted != null" class="text-left">
+                              Parte del corpo</th>
+                            <th v-if="item.discovered_at_age != null" class="text-left">
+                              Scoperto all'età di</th>
+                            <th v-if="item.note != null" class="text-left">Note</th>
                             <th colspan="2"/>
                           </tr>
                     </thead>
                     <tbody>
                         <tr>
-                        <td v-if="item.parental_type" class="text-left">{{item.parental_type}}</td>
-                <td v-if="item.body_impacted" class="text-left">{{item.body_impacted}}</td>
-                        <td v-if="item.discovered_at_age" class="text-left">
+                        <td v-if="item.parental_type != null" class="text-left">
+                          {{item.parental_type}}</td>
+                <td v-if="item.body_impacted != null" class="text-left">{{item.body_impacted}}</td>
+                        <td v-if="item.discovered_at_age != null" class="text-left">
                             {{item.discovered_at_age}}</td>
-                        <td v-if="item.note" class="text-left">{{item.note}}</td>
+                        <td v-if="item.note != null" class="text-left">{{item.note}}</td>
                         <td colspan="4"/>
                         </tr>
                     </tbody>
